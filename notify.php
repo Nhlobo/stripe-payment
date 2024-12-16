42693caef7e93b18a573bf583ce2e68f4eb747d5
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-12-16 12:31:08 +0200

    Update notify.php

--- notify.php
+++ notify.php
@@ -1,11 +1,8 @@
 <?php
-// notify.php
-
-// PayFast Notification URL
-// Receive the payment notification sent by PayFast
+// PayFast Notification URL - Receive the payment notification sent by PayFast
 $notification = file_get_contents('php://input');
 
-// Process the notification here (Verify the transaction, update order status, etc.)
+// Process the notification (Verify the transaction, update order status, etc.)
 file_put_contents('payment_log.txt', $notification, FILE_APPEND);
 
 // Return a success response to PayFast
